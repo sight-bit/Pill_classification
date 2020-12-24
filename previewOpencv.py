8cd0e3203c7fc9cc3c676938252b3c3091aa5cd8
imkyjkk <imkyjkk@gmail.com>
2020-12-24 10:37:46 +0900

    use for light control

---
+++ previewOpencv.py
@@ -0,0 +1,7 @@
+import AdapterBoard 
+
+Arducam_adapter_board = AdapterBoard.MultiAdapter()
+if __name__ == "__main__":
+    Arducam_adapter_board.save_pic(320,240,'A') # up
+    # Arducam_adapter_board.save_pic(320,240,'C') # down
+    
