adc20ac07ced19955695863ad93c8cace402e53a
sight-bit <76139189+sight-bit@users.noreply.github.com>
2021-01-03 03:59:49 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ def infer(frame_up, frame_down):
     net_input = np.hstack((frame_up,frame_down))    
     tensorflowNet.setInput(cv.dnn.blobFromImage(net_input, 1/255, size=(224,224))) # 1 3 224 224 # NCHW  #swapRB=True, crop=False
     networkOutput = tensorflowNet.forward()
-    idx = np.argmax(networkOutput) # pill index
+    idx = np.argmax(networkOutput)+1 # pill index
     pn = r[idx][1] # pill name
     pg = r[idx][5] # pill guide 0 1 2
 
